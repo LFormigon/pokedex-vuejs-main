586a52a8ad4f222f09545b2557b6fa7b9ca486a3
Leonardo Formigon <leonardofajoli@gmail.com>
2023-09-17 22:00:42 -0300

    .

--- src/components/ListPokemons.vue
+++ src/components/ListPokemons.vue
@@ -1,65 +1,62 @@
 <template>
-    <div class="col-4">
-      <div class="card mb-3">
-        <img class="card-img-top" :src="'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/' + pokemonId + '.png'" alt="Card image">
-        <div class="card-body d-flex flex-column justify-content-center align-items-center">
-          <h5 class="card-title text-center text-capitalize">{{ pokemonData ? pokemonData.name : 'Carregando...' }}</h5>
-          <p class="card-text text-center">Descubra mais sobre o Pokémon {{ pokemonData ? pokemonData.name : 'Carregando...' }} em nossa pokédex</p>
-          <button class="btn btn-dark" @click="showAbilities = !showAbilities">Detalhes</button>
-          <!-- Exibir informações de gênero e habilidades quando showAbilities for verdadeiro -->
-          <div v-if="showAbilities">
-            <p class="text-center">Gênero: {{ getpokemonGender() }}</p>
-            <p class="text-center">Habilidades:</p>
-            
-  
-            <ul class="text-center">
-              <li v-for="(ability, index) in abilitiesData" :key="index">{{ ability.name }}</li>
-            </ul>
-          </div>
+  <div class="col-4">
+    <div class="card mb-3">
+      <img class="card-img-top" :src="'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/' + pokemonId + '.png'" alt="Card image">
+      <div class="card-body d-flex flex-column justify-content-center align-items-center">
+        <h5 class="card-title text-center text-capitalize">{{ pokemonData ? pokemonData.name : pokemon.name }}</h5>
+        <p class="card-text text-center">Descubra mais sobre o Pokémon {{ pokemonData ? pokemonData.name : 'Carregando...' }} em nossa pokédex</p>
+        <button class="btn btn-dark" @click="showAbilities = !showAbilities">Detalhes</button>
+        <div v-if="showAbilities">
+          <p class="text-center">Gênero: {{ getpokemonGender() }}</p>
+          <p class="text-center">Habilidades:</p>
+          <ul class="text-center">
+            <li v-for="(ability, index) in abilitiesData" :key="index">{{ ability.name }}</li>
+          </ul>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script setup>
-  import { defineProps, ref, onMounted } from 'vue';
-  
-  const pokemon = defineProps(['name', 'link', 'abilities', 'gender']);
-  const pokemonId = pokemon.link.split("/")[6].toString().padStart(3, '0');
-  const showAbilities = ref(false);
-  
-  const pokemonData = ref(null);
-  const abilitiesData = ref([]);
-  
-  const fetchPokemonData = async () => {
-    try {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`);
-      const data = await response.json();
-      pokemonData.value = data;
-    } catch (error) {
-      console.error("Erro ao buscar informações do Pokémon", error);
-    }
-  };
-  
-  const fetchAbilitiesData = async () => {
-    try {
-      const abilities = pokemon.abilities.map((ability) => ability.ability.url);
-      const response = await Promise.all(abilities.map((url) => fetch(url)));
-      const data = await Promise.all(response.map((res) => res.json()));
-      abilitiesData.value = data;
-    } catch (error) {
-      console.error("Erro ao buscar informações de habilidades do Pokémon", error);
-    }
-  };
-  
-  const getpokemonGender = () => {
-    return pokemonData ? (pokemonData.gender ? pokemonData.gender.name : 'Desconhecido') : 'Desconhecido';
-  };
-  
-  onMounted(() => {
-    fetchPokemonData();
-    fetchAbilitiesData();
-  });
-  </script>
-  
-  <style></style>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, ref, onMounted } from 'vue';
+
+const pokemon = defineProps(['name', 'link', 'abilities', 'gender']);
+const pokemonId = pokemon.link.split("/")[6].toString().padStart(3, '0');
+const showAbilities = ref(false);
+
+const pokemonData = ref(null);
+const abilitiesData = ref([]);
+
+const fetchPokemonData = async () => {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`);
+    const data = await response.json();
+    pokemonData.value = data;
+  } catch (error) {
+    console.error("Erro ao buscar informações do Pokémon", error);
+  }
+};
+
+const fetchAbilitiesData = async () => {
+  try {
+    const abilities = pokemon.abilities.map((ability) => ability.ability.url);
+    const response = await Promise.all(abilities.map((url) => fetch(url)));
+    const data = await Promise.all(response.map((res) => res.json()));
+    abilitiesData.value = data;
+  } catch (error) {
+    console.error("Erro ao buscar informações de habilidades do Pokémon", error);
+  }
+};
+
+const getpokemonGender = () => {
+  return pokemonData.value ? (pokemonData.value.gender ? pokemonData.value.gender.name : 'Desconhecido') : 'Desconhecido';
+};
+
+onMounted(() => {
+  fetchPokemonData();
+  fetchAbilitiesData();
+});
+</script>
+
+<style></style>
