cf87adcc0c60013ad04c9dbcd4cb274f23a249c6
Leonardo Formigon <leonardofajoli@gmail.com>
2023-09-22 20:38:55 -0300

    t

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,52 +3,67 @@ import { onMounted, reactive, ref } from 'vue';
 import ListPokemons from '../components/ListPokemons.vue'
 
 
-let pokemons = reactive(ref());
+
+
+let pokemons = reactive(ref([]));
+
 
 onMounted(() => {
   fetch("https://pokeapi.co/api/v2/pokemon/?limit=9&offset=0")
-  .then(res => res.json())
-  .then(res => {
-    pokemons.value = res.results;
-    console.log(res)
-  })
+    .then(pokemonsList => pokemonsList.json())
+    .then(pokemonsList => {
+      pokemonsList.results.map((e) => {
+        fetch("https://pokeapi.co/api/v2/pokemon/" + e.name)
+          .then(pokemonDetails => pokemonDetails.json())
+          .then(pokemonDetails => {
+            pokemons.value.push({
+              name: pokemonDetails.name,
+              id: pokemonDetails.id,
+              types: pokemonDetails.types.map((e) => e.type),
+            })
+
+
+            console.log(pokemons.value)
+          })
+      })
+    })
+
+
 });
+
 
 </script>
 
+
 <template>
-        <v-row>
-          <v-container>
-            <h1 class="text-center white--text mb-2" style="font-size: 5rem">
-              Pokédex
-            </h1>
-            <h1 class="text-center white--text mb-8">
-              Created by
-              <a class="red--text" href="https://github.com/LFormigon">Formigon</a>
-            </h1>
-          </v-container>
-        </v-row>
+  <v-row>
+    <v-container>
+      <h1 class="text-center white--text mb-2" style="font-size: 5rem">
+        Pokédex
+      </h1>
+      <h1 class="text-center white--text mb-8">
+        Created by
+        <a class="red--text" href="https://github.com/LFormigon">Formigon</a>
+      </h1>
+    </v-container>
+  </v-row>
+
 
   <main>
     <div class="container">
       <div class="d-flex justify-content-center align-items-center">
         <div class="col-sm-12 col-md-7">
-            <div class="card-body row">
-              <ListPokemons 
-                v-for="pokemon in pokemons"
-                :key="pokemon.name"
-                :name="pokemon.name"
-                :link="pokemon.url"
-                :showDetails="pokemon.showDetails"
-                :gender="pokemon.gender"
-                :abilities="pokemon.abilities"
-              />
-            </div>
+          <div class="card-body row">
+            <ListPokemons v-for="pokemon in pokemons" :key="pokemon.name" :name="pokemon.name" :id="pokemon.id" />
           </div>
         </div>
       </div>
+    </div>
   </main>
 </template>
+
+
+
 
 
 
@@ -57,8 +72,10 @@ onMounted(() => {
   color: red;
 }
 
-.white--text{
+
+.white--text {
   color: white;
+
 
 }
 </style>
